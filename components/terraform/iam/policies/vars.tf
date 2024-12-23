6a4cb3c6ddb28f991289daa64d3bcdf5ea0be8c9
Vaishnavir9901 <vaishnavi.putluru@nationalarchives.gov.uk>
2024-12-23 21:30:09 +0000

    Updated Infrastrcuture for web

--- components/terraform/iam/policies/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "deployment_s3_bucket" {}
-variable "logfile_s3_bucket" {}
-
-variable "rp_deployment_s3_root" {}
-variable "rp_logfile_s3_root" {}
-
-variable "service" {}
-
-variable "account_id" {}
