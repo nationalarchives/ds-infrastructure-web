7e981552ab3ba355ab763a59a373b62913f86e9c
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-01-09 15:50:56 +0000

    Remove E2E test suite

--- test/tests/explore-the-collection.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("explore the collection home page", async ({ page }) => {
-  await page.goto("/explore-the-collection/");
-
-  await expect(page.locator("h1")).toHaveText(/Explore the collection/);
-});
