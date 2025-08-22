78e5934b301e8b77ad19efef950c7c34a40d3dcf
Vaishnavi Reddy Putluru <vaishnavireddyputluru@Vaishnavis-MacBook-Pro.local>
2025-08-22 10:38:37 +0100

    Search scheduled cron

--- components/terraform/search/scripts/userdata.sh
+++ components/terraform/search/scripts/userdata.sh
@@ -23,6 +23,18 @@ sudo touch /var/log/start-up.log
 
 echo "$(date '+%Y-%m-%d %T') - system update" | sudo tee -a /var/log/start-up.log > /dev/null
 sudo dnf -y update
+
+sudo dnf -y install cronie 
+sudo systemctl enable crond
+sudo systemctl start crond
+
+sudo bash -c '(
+    crontab -l 2>/dev/null
+    grep -Fq "/usr/local/bin/run_daily_task.sh" /var/spool/cron/root || echo "0 11 * * * /usr/local/bin/run_daily_task.sh >> /var/log/daily_task.log 2>&1"
+    grep -Fq "/usr/local/bin/run_weekly_task.sh" /var/spool/cron/root || echo "0 8 * * 1 /usr/local/bin/run_weekly_task.sh >> /var/log/weekly_task.log 2>&1"
+) | crontab -'
+
+
 # included parameter store for automation in startup.sh
 echo "$(date '+%Y-%m-%d %T') - call startup script" | sudo tee -a /var/log/start-up.log > /dev/null
 /usr/local/bin/startup.sh
