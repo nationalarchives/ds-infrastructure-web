84e90bd568d3396c5085164f1548564646960e06
karlkern <karl.kern@nationalarchives.gov.uk>
2025-10-27 14:57:46 +0000

    comment ut module

--- components/terraform/request-service-record.tf
+++ components/terraform/request-service-record.tf
@@ -16,46 +16,46 @@ variable "request_service_record_deployment_s3_bucket" {}
 variable "request_service_record_folder_s3_key" {}
 
 
-module "mod-foi-frontend" {
-    source = "request-service-record"
-
-    environment = var.environment
-    ami_id = data.aws_ami.request_service_record_ami.id
-
-    route53_zone = data.aws_ssm_parameter.zone_id.value
-
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
-    private_subnet_a_id = data.aws_ssm_parameter.private_subnet_2a_id.value
-    private_subnet_b_id = data.aws_ssm_parameter.private_subnet_2b_id.value
-
-    asg_max_size = var.request_service_record_asg_max_size
-    asg_min_size = var.request_service_record_asg_min_size
-    asg_desired_capacity = var.request_service_record_asg_desired_capacity
-    asg_health_check_grace_period = var.request_service_record_asg_health_check_grace_period
-    asg_health_check_type = var.request_service_record_asg_health_check_type
-
-    instance_type = var.request_service_record_instance_type
-    key_name = "request-service-record-${var.environment}-eu-west-2"
-    root_block_device_size = "100"
-
-    instance_cidr = [
-        data.aws_ssm_parameter.private_subnet_2a_cidr.value,
-        data.aws_ssm_parameter.private_subnet_2b_cidr.value,
-    ]
-    lb_cidr = [
-        data.aws_ssm_parameter.private_subnet_2a_cidr.value,
-        data.aws_ssm_parameter.private_subnet_2b_cidr.value,
-    ]
-
-
-    auto_switch_off = var.request_service_record_auto_switch_off
-    auto_switch_on = var.request_service_record_auto_switch_on
-    deployment_group = var.request_service_record_deployment_group
-    patch_group = var.request_service_record_patch_group
-
-    deployment_s3_bucket = var.request_service_record_deployment_s3_bucket
-    folder_s3_key = var.request_service_record_folder_s3_key
-
-    asg_tags = local.asg_default_tags
-    tags = local.tags
-}
+#module "mod-foi-frontend" {
+#    source = "./request-service-record"
+#
+#    environment = var.environment
+#    ami_id = data.aws_ami.request_service_record_ami.id
+#
+#    route53_zone = data.aws_ssm_parameter.zone_id.value
+#
+#    vpc_id = data.aws_ssm_parameter.vpc_id.value
+#    private_subnet_a_id = data.aws_ssm_parameter.private_subnet_2a_id.value
+#    private_subnet_b_id = data.aws_ssm_parameter.private_subnet_2b_id.value
+#
+#    asg_max_size = var.request_service_record_asg_max_size
+#    asg_min_size = var.request_service_record_asg_min_size
+#    asg_desired_capacity = var.request_service_record_asg_desired_capacity
+#    asg_health_check_grace_period = var.request_service_record_asg_health_check_grace_period
+#    asg_health_check_type = var.request_service_record_asg_health_check_type
+#
+#    instance_type = var.request_service_record_instance_type
+#    key_name = "request-service-record-${var.environment}-eu-west-2"
+#    root_block_device_size = "100"
+#
+#    instance_cidr = [
+#        data.aws_ssm_parameter.private_subnet_2a_cidr.value,
+#        data.aws_ssm_parameter.private_subnet_2b_cidr.value,
+#    ]
+#    lb_cidr = [
+#        data.aws_ssm_parameter.private_subnet_2a_cidr.value,
+#        data.aws_ssm_parameter.private_subnet_2b_cidr.value,
+#    ]
+#
+#
+#    auto_switch_off = var.request_service_record_auto_switch_off
+#    auto_switch_on = var.request_service_record_auto_switch_on
+#    deployment_group = var.request_service_record_deployment_group
+#    patch_group = var.request_service_record_patch_group
+#
+#    deployment_s3_bucket = var.request_service_record_deployment_s3_bucket
+#    folder_s3_key = var.request_service_record_folder_s3_key
+#
+#    asg_tags = local.asg_default_tags
+#    tags = local.tags
+#}
