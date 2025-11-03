8ff4d9c130042038bcdf850e4c5546da0422b545
karlkern <karl.kern@nationalarchives.gov.uk>
2025-11-03 11:25:04 +0000

    remove iam parts from main iam

--- components/terraform/iam/roles/roles.tf
+++ components/terraform/iam/roles/roles.tf
@@ -240,33 +240,33 @@ resource "aws_iam_role_policy_attachment" "wagtaildocs_policy_attachment_6" {
 
 # Request Service Record Role - MoD FoI
 #
-resource "aws_iam_role" "request_service_record_role" {
-    name = "request-service-record-role"
-    assume_role_policy = file("${path.root}/shared-templates/ec2_assume_role.json")
-}
-resource "aws_iam_instance_profile" "request_service_record_profile" {
-    name = "request-service-record-profile"
-    path = "/"
-    role = aws_iam_role.request_service_record_role.name
-}
-# Attach Policies to Request Service Record Role
-resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_1" {
-    role       = aws_iam_role.request_service_record_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
-}
-resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_2" {
-    role       = aws_iam_role.request_service_record_role.name
-    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_3" {
-    role       = aws_iam_role.request_service_record_role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_4" {
-    role       = aws_iam_role.request_service_record_role.name
-    policy_arn = var.org_level_logging_arn
-}
-resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_5" {
-    role       = aws_iam_role.request_service_record_role.name
-    policy_arn = var.deployment_s3_policy
-}
+#resource "aws_iam_role" "request_service_record_role" {
+#    name = "request-service-record-role"
+#    assume_role_policy = file("${path.root}/shared-templates/ec2_assume_role.json")
+#}
+#resource "aws_iam_instance_profile" "request_service_record_profile" {
+#    name = "request-service-record-profile"
+#    path = "/"
+#    role = aws_iam_role.request_service_record_role.name
+#}
+## Attach Policies to Request Service Record Role
+#resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_1" {
+#    role       = aws_iam_role.request_service_record_role.name
+#    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+#}
+#resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_2" {
+#    role       = aws_iam_role.request_service_record_role.name
+#    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+#}
+#resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_3" {
+#    role       = aws_iam_role.request_service_record_role.name
+#    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#}
+#resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_4" {
+#    role       = aws_iam_role.request_service_record_role.name
+#    policy_arn = var.org_level_logging_arn
+#}
+#resource "aws_iam_role_policy_attachment" "rsr_policy_attachment_5" {
+#    role       = aws_iam_role.request_service_record_role.name
+#    policy_arn = var.deployment_s3_policy
+#}
