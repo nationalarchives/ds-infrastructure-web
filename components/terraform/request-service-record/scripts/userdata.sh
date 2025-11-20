ec05875aba1d0df1447712f72d9d3d0d8746d197
Vaishnavi Reddy Putluru <vaishnavireddyputluru@Vaishnavis-MacBook-Pro.local>
2025-11-20 15:21:41 +0000

    Add AWS keys for MODFOI

--- components/terraform/request-service-record/scripts/userdata.sh
+++ components/terraform/request-service-record/scripts/userdata.sh
@@ -22,6 +22,21 @@ Content-Disposition: attachment; filename="userdata.txt"
 sudo touch /var/log/start-up.log
 
 echo "$(date '+%Y-%m-%d %T') - system update" | sudo tee -a /var/log/start-up.log > /dev/null
+
+sudo yum install -y cronie
+sudo systemctl enable crond
+sudo systemctl start crond
+chmod +x /usr/local/bin/refresh_aws_keys.sh
+sudo touch /var/log/refresh_aws_keys.log
+sudo chmod 664 /var/log/refresh_aws_keys.log
+sudo chown $(whoami): /var/log/refresh_aws_keys.log
+
+sudo /usr/local/bin/refresh_aws_keys.sh
+
+(
+    sudo crontab -l 2>/dev/null | grep -v '/usr/local/bin/refresh_aws_keys.sh'
+    echo "*/30 * * * * /usr/local/bin/refresh_aws_keys.sh >> /var/log/refresh_aws_keys.log 2>&1"
+) | sudo crontab -
 sudo dnf -y update
 # included parameter store for automation in startup.sh
 echo "$(date '+%Y-%m-%d %T') - call startup script" | sudo tee -a /var/log/start-up.log > /dev/null
