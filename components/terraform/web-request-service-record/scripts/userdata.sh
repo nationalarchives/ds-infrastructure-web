6ee53aac19b30680e903e0dbb2d335c7c07f0d61
Vaishnavi Reddy Putluru <vaishnavireddyputluru@Vaishnavis-MacBook-Pro.local>
2026-01-06 12:44:32 +0000

    delete rsr aws creds

--- components/terraform/web-request-service-record/scripts/userdata.sh
+++ components/terraform/web-request-service-record/scripts/userdata.sh
@@ -26,17 +26,7 @@ echo "$(date '+%Y-%m-%d %T') - system update" | sudo tee -a /var/log/start-up.lo
 sudo yum install -y cronie
 sudo systemctl enable crond
 sudo systemctl start crond
-chmod +x /usr/local/bin/refresh_aws_keys.sh
-sudo touch /var/log/refresh_aws_keys.log
-sudo chmod 664 /var/log/refresh_aws_keys.log
-sudo chown $(whoami): /var/log/refresh_aws_keys.log
 
-sudo /usr/local/bin/refresh_aws_keys.sh
-
-(
-    sudo crontab -l 2>/dev/null | grep -v '/usr/local/bin/refresh_aws_keys.sh'
-    echo "*/30 * * * * /usr/local/bin/refresh_aws_keys.sh >> /var/log/refresh_aws_keys.log 2>&1"
-) | sudo crontab -
 sudo dnf -y update
 # included parameter store for automation in startup.sh
 echo "$(date '+%Y-%m-%d %T') - call startup script" | sudo tee -a /var/log/start-up.log > /dev/null
