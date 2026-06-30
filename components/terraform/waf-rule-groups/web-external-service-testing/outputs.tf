4eeb36fbd48b7808c3bb3c5be39e18a4040c2ad2
karlkern <karl.kern@nationalarchives.gov.uk>
2026-06-30 14:58:03 +0100

    set output

--- components/terraform/waf-rule-groups/web-external-service-testing/outputs.tf
+++ components/terraform/waf-rule-groups/web-external-service-testing/outputs.tf
@@ -1,3 +1,3 @@
-output "web_api_access_rule_group_arn" {
-    value = aws_wafv2_rule_group.web_api_access.arn
+output "web_external_service_testing_rule_group_arn" {
+    value = aws_wafv2_rule_group.web_external_service_testing.arn
 }
