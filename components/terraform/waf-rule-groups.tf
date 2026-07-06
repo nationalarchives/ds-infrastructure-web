ba5ac40cfdb602810b04c9fc950c41fcea63af5a
karlkern <karl.kern@nationalarchives.gov.uk>
2026-07-06 15:44:34 +0100

    try import in code

--- components/terraform/waf-rule-groups.tf
+++ components/terraform/waf-rule-groups.tf
@@ -1,5 +1,5 @@
 import {
-  to = module.emergency_group.aws_wafv2_rule_group.web_emergency_rg
+  to = module.emergency_group[0].aws_wafv2_rule_group.web_emergency_rg
   id = "df52d9dd-ec4a-4c18-8b25-f414aa0452fc/web-emergency-rg/CLOUDFRON"
 }
 
