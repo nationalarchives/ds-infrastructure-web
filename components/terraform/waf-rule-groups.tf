e20f85dfb9d58b301e72360214a1ba9e8bd757fa
karlkern <karl.kern@nationalarchives.gov.uk>
2026-07-06 15:53:26 +0100

    try import in code

--- components/terraform/waf-rule-groups.tf
+++ components/terraform/waf-rule-groups.tf
@@ -1,7 +1,6 @@
 import {
   to = module.emergency_group[0].aws_wafv2_rule_group.web_emergency_rg
-  id = "df52d9dd-ec4a-4c18-8b25-f414aa0452fc/web-emergency-rg/CLOUDFRONT"
-    region = "us-east-1"
+  id = "df52d9dd-ec4a-4c18-8b25-f414aa0452fc/web-emergency-rg/Global"
 }
 
 module "emergency_group" {
